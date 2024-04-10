c40969f12b0ece3410666c0ad7dccf5d97dc0886
Gabriel Caires <gabriel.caires@automattic.com>
2024-04-10 16:54:59 +0100

    First commit

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,5 +5,30 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.dracula-nvim" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.git.git-blame-nvim" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.project.nvim-spectre" },
+  { import = "astrocommunity.editing-support.yanky-nvim" },
+  { import = "astrocommunity.scrolling.mini-animate" },
+  { import = "astrocommunity.git.gitlinker-nvim" },
+  { import = "astrocommunity.git.gitlinker-nvim" },
+  { import = "astrocommunity.remote-development.distant-nvim" },
+ {"OlegGulevskyy/better-ts-errors.nvim",
+  dependencies = { "MunifTanjim/nui.nvim" },
+  config = {
+    keymaps = {
+      toggle = '<leader>dd', -- default '<leader>dd'
+      go_to_definition = '<leader>dx' -- default '<leader>dx'
+    }
+  }
+
+  },
+  
 }
